679341a5b4cf357863f5c60d923dedf652a71a7b
amabange97 <amabangejesse@gmail.com>
2024-09-25 22:30:41 +0000

    onboarding ui wip

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import App from "./App.tsx";
+import "./index.css";
+import Home from "./pages/Home.tsx";
+import Form1 from "./pages/Form1.tsx";
+import Dashboard from "./pages/Dashboard/App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* <App /> */}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Form1" element={<Form1 />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  </StrictMode>
+);
