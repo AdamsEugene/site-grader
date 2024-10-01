56e941a6b73de436f3de4ec213af6b5fe6bd33e6
amabange97 <amabangejesse@gmail.com>
2024-10-01 08:43:56 +0000

    fixed app link button merge conflicts

--- src/components/AppLinkButton.tsx
+++ src/components/AppLinkButton.tsx
@@ -1,50 +1,21 @@
-<<<<<<< HEAD
-import { Link, LinkProps } from "react-router-dom";
-import { forwardRef, ReactElement } from "react";
-=======
-import { AnchorHTMLAttributes, ReactElement } from "react";
->>>>>>> 4ecc786 (dashboard progress)
+import { LinkProps } from "react-router-dom";
+// import { forwardRef, ReactElement } from "react";
+// import { AnchorHTMLAttributes, ReactElement } from "react";
 
 interface AppLinkButtonProps extends LinkProps {
   label?: string;
   className?: string;
   primary?: boolean;
-  leftIcon?: ReactElement;
-  rightIcon?: ReactElement;
+  // leftIcon?: ReactElement;
+  // rightIcon?: ReactElement;
 }
 
-<<<<<<< HEAD
-const AppLinkButton = forwardRef<HTMLAnchorElement, AppLinkButtonProps>(
-  ({ label, className = "", primary, rightIcon, leftIcon, ...props }, ref) => {
-    return (
-      <Link
-        ref={ref}
-        className={`${
-          primary
-            ? "disabled:bg-emerald-700/50 border-0 bg-emerald-700 hover:bg-emerald-700/80 text-white"
-            : "border border-gray-300 hover:bg-gray-100"
-        } disabled:bg-green-200 disabled:text-gray-400 rounded-lg p-2 px-4 cursor-pointer text-sm disabled:cursor-default inline-flex relative items-center justify-center ${className}`}
-        aria-label={label ? undefined : "Link button"}
-        {...props}
-      >
-        {/* <div className="flex relative items-center justify-center"> */}
-        {leftIcon}
-        {label}
-        {rightIcon}
-        {/* </div> */}
-      </Link>
-    );
-  }
-);
-
-export default AppLinkButton;
-=======
 export default function AppLinkButton({
   label,
   className = "",
   primary,
-  rightIcon,
-  leftIcon,
+  // rightIcon,
+  // leftIcon,
   ...props
 }: AppLinkButtonProps) {
   return (
@@ -57,11 +28,10 @@ export default function AppLinkButton({
       {...props}
     >
       <div className="flex relative items-center justify-center">
-        {leftIcon}
+        {/* {leftIcon} */}
         {label}
-        {rightIcon}
+        {/* {rightIcon} */}
       </div>
     </a>
   );
 }
->>>>>>> 4ecc786 (dashboard progress)
