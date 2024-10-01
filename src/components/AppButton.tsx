796904ddf7732632f3939322b83c4a183a674904
amabange97 <amabangejesse@gmail.com>
2024-10-01 09:34:07 +0000

    alias fix on appbutton

--- src/components/AppButton.tsx
+++ src/components/AppButton.tsx
@@ -8,28 +8,26 @@ interface AppButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   rightIcon?: ReactElement;
 }
 
-export default function AppButton({
-  label,
-  className = "",
-  primary,
-  rightIcon,
-  leftIcon,
-  ...props
-}: AppButtonProps) {
-  return (
-    <button
-      className={`${
-        primary
-          ? "disabled:bg-emerald-700/50 border-0 bg-emerald-700 hover:bg-emerald-700/80 text-white"
-          : "border border-gray-300 hover:bg-gray-100"
-      } disabled:bg-green-200 disabled:text-gray-400 rounded-lg p-2 px-4 cursor-pointer text-sm disabled:cursor-default ${className}`}
-      {...props}
-    >
-      <div className="flex items-center justify-center">
-        {leftIcon}
-        {label}
-        {rightIcon}
-      </div>
-    </button>
-  );
-}
+const AppButton = forwardRef<HTMLButtonElement, AppButtonProps>(
+  ({ label, className = "", primary, rightIcon, leftIcon, ...props }, ref) => {
+    return (
+      <button
+        ref={ref}
+        className={`${
+          primary
+            ? "disabled:bg-emerald-700/50 border-0 bg-emerald-700 hover:bg-emerald-700/80 text-white"
+            : "border border-gray-300 hover:bg-gray-100"
+        } disabled:bg-green-200 disabled:text-gray-400 rounded-lg p-2 px-4 cursor-pointer text-sm disabled:cursor-default ${className}`}
+        {...props}
+      >
+        <div className="flex items-center justify-center">
+          {leftIcon}
+          {label}
+          {rightIcon}
+        </div>
+      </button>
+    );
+  }
+);
+
+export default AppButton;
